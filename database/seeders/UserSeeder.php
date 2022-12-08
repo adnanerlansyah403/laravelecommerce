f62ebcd8ac68d3cb62bd197baa3d849e2256dcee
adnanerlansyah403 <adnanerlansyah403@gmail.com>
2022-12-08 20:47:12 +0700

    first commit project ecommerce

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Admin 
+
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('admin'),
+                'role' => 'admin',
+                'status' => 'active',   
+            ],
+            [
+                'name' => 'Ariyan Vendor',
+                'username' => 'vendor',
+                'email' => 'vendor@example.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => 'active',    
+            ],
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@example.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => 'active',
+            ]
+        ]);
+    }
+}
